9c55d5526f82bc1f29466c8ce199fb69984b3f7c
Антон Удод <udod@aigeo.ru>
2020-10-28 07:28:04 +0700

    added firebase as backend(files)

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBbDJGS3y69-Wo6bKR0k1barDFCfEUOOCw",
+  authDomain: "test-app-book-catalog.firebaseapp.com",
+  databaseURL: "https://test-app-book-catalog.firebaseio.com",
+  projectId: "test-app-book-catalog",
+  storageBucket: "test-app-book-catalog.appspot.com",
+  messagingSenderId: "823881484135",
+  appId: "1:823881484135:web:72986b41c452450d2391c2"
+};
+
+export default firebaseConfig;
